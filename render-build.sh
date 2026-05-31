9c706fa7d013620d844405641e6a5c1b2b1b15e0
ChristianRenzLedesma <renzledesma76@gmail.com>
2026-05-31 19:21:55 +0800

    Add Railway config with nixpacks

--- render-build.sh
+++ render-build.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Install PHP and extensions
-apt-get update && apt-get install -y php8.2 php8.2-cli php8.2-mbstring php8.2-xml \
-    php8.2-zip php8.2-mysql php8.2-curl php8.2-gd php8.2-bcmath unzip curl
-
-# Install Composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-# Install PHP dependencies
-composer install --no-dev --optimize-autoloader
-
 # Install Node dependencies and build assets
-npm ci && npm run build
-
-# Cache Laravel config
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+npm ci
+npm run build
